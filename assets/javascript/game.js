dd7ddec5114f4cc5829a2094ebfbd7557bd2fbed
Patience Condell <condellp@gmail.com>
2019-09-24 23:28:26 -0400

    final touch

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-    var Random = Math.floor(Math.random() * 102 + 19)
+    var Random = Math.floor(Math.random() * 60 + 60)
     $('#scoreToMatch').text(Random);
 
 
-    var num1 = Math.floor(Math.random() * 12 + 1)
-    var num2 = Math.floor(Math.random() * 12 + 1)
-    var num3 = Math.floor(Math.random() * 12 + 1)
-    var num4 = Math.floor(Math.random() * 12 + 1)
+    var num1 = Math.floor(Math.random() * 10 + 3)
+    var num2 = Math.floor(Math.random() * 10 + 3)
+    var num3 = Math.floor(Math.random() * 10 + 3)
+    var num4 = Math.floor(Math.random() * 10 + 3)
 
-    var playerTotal = 0;
+    var userTotal = 0;
     var wins = 0;
     var losses = 0;
 
@@ -18,15 +18,15 @@ $(document).ready(function () {
 
 
     function reset() {
-        Random = Math.floor(Math.random() * 102 + 19);
+        Random = Math.floor(Math.random() * 60 + 60);
         console.log(Random)
         $('#scoreToMatch').text(Random);
-        num1 = Math.floor(Math.random() * 12 + 1);
-        num2 = Math.floor(Math.random() * 12 + 1);
-        num3 = Math.floor(Math.random() * 12 + 1);
-        num4 = Math.floor(Math.random() * 12 + 1);
-        playerTotal = 0;
-        $('#totalScore').text(playerTotal);
+        num1 = Math.floor(Math.random() * 10 + 3);
+        num2 = Math.floor(Math.random() * 10 + 3);
+        num3 = Math.floor(Math.random() * 10 + 3);
+        num4 = Math.floor(Math.random() * 10 + 3);
+        userTotal = 0;
+        $('#totalScore').text(userTotal);
     }
 
     function winner() {
@@ -44,46 +44,46 @@ $(document).ready(function () {
     }
 
     $('.red').on('click', function () {
-        playerTotal = playerTotal + num1;
-        console.log("New playerTotal= " + playerTotal);
-        $('#totalScore').text(playerTotal);
-        if (playerTotal == Random) {
+        userTotal = userTotal + num1;
+        console.log("New userTotal= " + userTotal);
+        $('#totalScore').text(userTotal);
+        if (userTotal == Random) {
             winner();
         }
-        else if (playerTotal > Random) {
+        else if (userTotal > Random) {
             loser();
         }
     })
     $('.blue').on('click', function () {
-        playerTotal = playerTotal + num2;
-        console.log("New playerTotal= " + playerTotal);
-        $('#totalScore').text(playerTotal);
-        if (playerTotal == Random) {
+        userTotal = userTotal + num2;
+        console.log("New userTotal= " + userTotal);
+        $('#totalScore').text(userTotal);
+        if (userTotal == Random) {
             winner();
         }
-        else if (playerTotal > Random) {
+        else if (userTotal > Random) {
             loser();
         }
     })
     $('.yellow').on('click', function () {
-        playerTotal = playerTotal + num3;
-        console.log("New playerTotal= " + playerTotal);
-        $('#totalScore').text(playerTotal);
-        if (playerTotal == Random) {
+        userTotal = userTotal + num3;
+        console.log("New userTotal= " + userTotal);
+        $('#totalScore').text(userTotal);
+        if (userTotal == Random) {
             winner();
         }
-        else if (playerTotal > Random) {
+        else if (userTotal > Random) {
             loser();
         }
     })
     $('.green').on('click', function () {
-        playerTotal = playerTotal + num4;
-        console.log("New playerTotal= " + playerTotal);
-        $('#totalScore').text(playerTotal);
-        if (playerTotal == Random) {
+        userTotal = userTotal + num4;
+        console.log("New userTotal= " + userTotal);
+        $('#totalScore').text(userTotal);
+        if (userTotal == Random) {
             winner();
         }
-        else if (playerTotal > Random) {
+        else if (userTotal > Random) {
             loser();
         }
     });
